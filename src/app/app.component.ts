9544f455c33c2ca4a7c7ddbea32df2727f022b6d
glasner-it <glasner.it@gmail.com>
2023-01-17 18:26:20 +0100

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dr n. med. Leopold Glasner';
+  title = 'dr n. med. Leopold Glasner';
   isSmallScreen = false;
   getIsSmallScreen(): boolean {
     const w = document.documentElement.clientWidth;
